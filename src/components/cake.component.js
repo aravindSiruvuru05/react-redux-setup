dc913b67b8f9691beab297acd5d7846d1199cc2a
aravindSiruvuru05 <siruvuruaravind@gmail.com>
2020-06-23 21:34:23 +0530

    initial setup with installing react-redux and redux

---
+++ src/components/cake.component.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function CakeComponent() {
+    return (
+        <div>
+            <h1> Number of cakes</h1>
+            <button>BUY CAKES</button>
+        </div>
+    )
+}
